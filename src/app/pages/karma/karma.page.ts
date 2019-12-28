94c0ac08e476b665a01430f9fb4befbfa88e343e
Meet Shah <meetdshah007@gmail.com>
2019-12-28 17:33:57 +0530

    Fixed the calculation for the Ghada level.

--- src/app/pages/karma/karma.page.ts
+++ src/app/pages/karma/karma.page.ts
@@ -8,10 +8,10 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./karma.page.scss'],
 })
 export class KarmaPage implements OnInit, OnDestroy {
-  defaultMargin = '125%';
   percentage = `101%`;
   percentage2 = `101%`;
   percentage3 = `101%`;
+  defaultMargin = 140;
   actualPercentage = 100;
   actualPercentage2 = 100;
   actualPercentage3 = 100;
@@ -40,16 +40,29 @@ export class KarmaPage implements OnInit, OnDestroy {
       const count = this.actualPercentage;
       const count2 = this.actualPercentage2;
       const count3 = this.actualPercentage3;
-      this.percentage = `${count}%`;
-      this.percentage2 = `${count2}%`;
-      this.percentage3 = `${count3}%`;
+      this.percentage = `${this.countMarginInPercent(count)}%`;
+      this.percentage2 = `${this.countMarginInPercent(count2)}%`;
+      this.percentage3 = `${this.countMarginInPercent(count3)}%`;
     }, 300);
   }
 
+  countMarginInPercent(percent: number): string {
+    if (percent > 95) {
+      percent = 95;
+    }
+
+    if (percent === 100) {
+      percent = 100;
+    }
+
+    const defaultMargin = this.defaultMargin;
+    return ((defaultMargin * percent) / 100).toFixed(0);
+  }
+
   resetAll() {
-    this.percentage = this.defaultMargin;
-    this.percentage2 = this.defaultMargin;
-    this.percentage3 = this.defaultMargin;
+    this.percentage = `${this.defaultMargin}%`;
+    this.percentage2 = `${this.defaultMargin}%`;
+    this.percentage3 = `${this.defaultMargin}%`;
   }
 
   ngOnDestroy() {
